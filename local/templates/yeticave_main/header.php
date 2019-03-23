2adb910b8fc800a7757fdf93c0b3a45c8de8b2ae
AnVP <amltrp@mail.ru>
2019-03-23 18:58:31 +0500

    Перенос верстки в шаблон

---
+++ local/templates/yeticave_main/header.php
@@ -0,0 +1,53 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <?php
+        $APPLICATION -> ShowHead();
+        use \Bitrix\Main\Page\Asset;
+    ?>
+<!--  <meta charset="UTF-8">-->
+  <title><?php $APPLICATION -> ShowTitle();?></title>
+    
+    <?php
+        Asset::getInstance() -> addCss(SITE_TEMPLATE_PATH . '/css/normalize.min.css');
+        Asset::getInstance() -> addCss(SITE_TEMPLATE_PATH . '/css/style.css');
+    ?>
+    
+<!--  <link href="css/normalize.min.css" rel="stylesheet">-->
+<!--  <link href="css/style.css" rel="stylesheet">-->
+</head>
+<body>
+<div id="panel"><?php $APPLICATION -> ShowPanel(); ?></div>
+
+<div class="page-wrapper">
+
+  <header class="main-header">
+    <div class="main-header__container container">
+      <h1 class="visually-hidden">YetiCave</h1>
+      <a class="main-header__logo">
+        <img src="<?=SITE_TEMPLATE_PATH;?>/img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
+      </a>
+      <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru">
+        <input type="search" name="search" placeholder="Поиск лота">
+        <input class="main-header__search-btn" type="submit" name="find" value="Найти">
+      </form>
+      <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
+      <nav class="user-menu">
+        <div class="user-menu__image">
+          <img src="<?=SITE_TEMPLATE_PATH;?>/img/user.jpg" width="40" height="40" alt="Пользователь">
+        </div>
+        <div class="user-menu__logged">
+          <p>Константин</p>
+          <a href="login.html">Выйти</a>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <main class="container">
