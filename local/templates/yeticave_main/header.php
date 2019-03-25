7e7210380f33c592a2365be86cdefc06d740caf6
AnVP <amltrp@mail.ru>
2019-03-25 17:59:36 +0500

    Добавляет личный кабинет

--- local/templates/yeticave_main/header.php
+++ local/templates/yeticave_main/header.php
@@ -11,7 +11,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
         $APPLICATION -> ShowHead();
         use \Bitrix\Main\Page\Asset;
     ?>
-<!--  <meta charset="UTF-8">-->
+
   <title><?php $APPLICATION -> ShowTitle();?></title>
     
     <?php
@@ -19,8 +19,6 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
         Asset::getInstance() -> addCss(SITE_TEMPLATE_PATH . '/css/style.css');
     ?>
     
-<!--  <link href="css/normalize.min.css" rel="stylesheet">-->
-<!--  <link href="css/style.css" rel="stylesheet">-->
 </head>
 <body>
 <div id="panel"><?php $APPLICATION -> ShowPanel(); ?></div>
@@ -42,12 +40,15 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 		    )
 	    );?>
 	
-	    <?$APPLICATION->IncludeComponent("bitrix:search.form", "search", Array(
-	"PAGE" => "#SITE_DIR#search/index.php",	// Страница выдачи результатов поиска (доступен макрос #SITE_DIR#)
-		"USE_SUGGEST" => "N",	// Показывать подсказку с поисковыми фразами
-	),
-	false
-);?>
+	    <?$APPLICATION->IncludeComponent(
+	            "bitrix:search.form",
+                "search",
+                Array(
+	                "PAGE" => "#SITE_DIR#search/index.php",	// Страница выдачи результатов поиска (доступен макрос #SITE_DIR#)
+		            "USE_SUGGEST" => "N",	// Показывать подсказку с поисковыми фразами
+	            ),
+	            false
+        );?>
 	
 	    <?$APPLICATION->IncludeComponent(
 		    "bitrix:main.include",
@@ -59,16 +60,27 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 			    "PATH" => "/include/add_lot.php"
 		    )
 	    );?>
+	
+	    <?$APPLICATION->IncludeComponent(
+		    "bitrix:system.auth.form",
+		    "",
+		    Array(
+			    "FORGOT_PASSWORD_URL" => "",
+			    "PROFILE_URL" => "",
+			    "REGISTER_URL" => "",
+			    "SHOW_ERRORS" => "Y"
+		    )
+	    );?>
 
-        <nav class="user-menu">
+        <!--<nav class="user-menu">
             <div class="user-menu__image">
-                <img src="<?=SITE_TEMPLATE_PATH;?>/img/user.jpg" width="40" height="40" alt="Пользователь">
+                <img src="<?/*=SITE_TEMPLATE_PATH;*/?>/img/user.jpg" width="40" height="40" alt="Пользователь">
             </div>
             <div class="user-menu__logged">
                 <p>Константин</p>
                 <a href="login.html">Выйти</a>
             </div>
-        </nav>
+        </nav>-->
      
     </div>
   </header>
